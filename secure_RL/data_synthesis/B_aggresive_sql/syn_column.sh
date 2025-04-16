82ba27957937e66aa7f404a52dbe845287743677
cxb <cxb13717518263@163.com>
2025-04-16 19:42:20 +0800

    Add files via upload

--- secure_RL/data_synthesis/B_aggresive_sql/syn_column.sh
+++ secure_RL/data_synthesis/B_aggresive_sql/syn_column.sh
@@ -1,7 +1,7 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate torch_forself
-python syn.py \
---condition_path "C:\Users\Lenovo\torch\research\NL2SQL\secure_RL\data_synthesis\A_safe_condition\omni_whole_column_filtered.json" \
+python syn_infer.py \
+--condition_path "C:\Users\Lenovo\torch\research\NL2SQL\secure_RL\data_synthesis\A_safe_condition\omni_cells_filtered.json" \
 --prompts_path "./prompts.json" \
 --model_path "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B" \
 --db_dataset "C:\Users\Lenovo\torch\research\NL2SQL\secure_RL\data_synthesis\original_data\omni_2000.json" \
